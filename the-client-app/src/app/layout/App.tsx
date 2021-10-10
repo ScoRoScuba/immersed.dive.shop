62a2122141f9d4ae91b7dd2eda7191b5211c23ae
ScoRoScuba <scott.roberts@virgin.net>
2021-10-10 11:03:55 +1100

    Add real basic client

---
+++ the-client-app/src/app/layout/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css';
+import {Header} from 'semantic-ui-react';
+
+export default function App() {
+
+        return(
+            <div>
+                <Header as='h2' content='Immersed'/>
+
+            </div>                
+        );
+}
+
+
